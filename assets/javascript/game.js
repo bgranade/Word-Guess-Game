c8640016ecf9527dbad4d434c60a453075c7e4b6
bethgranade <bgranado488@gmail.com>
2019-10-10 16:02:13 -0500

    add js script to html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,3 +4,43 @@ let newArray=[];
 let totalChances = 0;
 let vocab= "";
 let Players=["Kobe", "Jordan", "Harden", "Duncan", "Curry", "Durant"]
+
+let Word = document.getElementById("word");
+let totalWins = document.getElementById("wins");
+let chancesLeft = document.getElementById("chances");
+let userGuess = document.getElementById("user-guess");
+let newMessage = document.getElementById("message");
+
+function addToArray(letter,display){
+    newArray.push({letter,display});
+}
+
+function buildArray(word){
+    for(let n of word){
+        if(n===" "){
+            addToArray(n," ");
+        }else{
+        addToArray(n,"_");
+        }
+    }
+}
+
+
+//start a new game
+function newGame(){
+    newMessage.textContent = ""
+    newArray=[];
+    vocab=Animals[(Math.floor((Math.random()) * Animals.length))]; 
+    console.log(vocab); 
+    buildArray(vocab);
+    wrongGuesses =[];
+    totalChances = 13;
+    userGuess.textContent = wrongGuesses;
+    chancesLeft.textContent= totalChances;
+    displayArray = newArray.map(function(entry){
+        for(i=0;i<vocab.length;i++){
+        return entry.display;
+        }});
+    Word.textContent=displayArray.join(" ");
+}
+
